2b8ff28a3f998f449201ef82977cf18950f5929c
Clansty <i@gao4.pw>
2023-11-10 16:31:51 +0800

    队列获取头像

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,6 @@ export default {
 				for (const userId of data.userIds) {
 					console.log(userId)
 					try {
-						await env.AVATAR_META.put(`updateTime:${userId}`, new Date().getTime().toString());
 						const avatarReq = await bot.getUserProfilePhotos(userId, 0, 1);
 						const avatars = (await avatarReq.json()) as any;
 						console.log(avatars);
@@ -58,15 +57,41 @@ export default {
 		for (let message of batch.messages) {
 			console.log(message.body)
 			try {
-				if (await env.ASSETS_STORE.head(`files/${message.body.fileId}`)) {
+				const bot = new Bot(message.body.botToken)
+				let fileId: string;
+				switch (message.body.type) {
+					case 'image':
+						fileId = message.body.fileId
+						break;
+					case "avatar":
+						console.log('获取头像文件 ID', message.body.userId)
+						const avatarReq = await bot.getUserProfilePhotos(message.body.userId, 0, 1);
+						const avatars = (await avatarReq.json()) as any;
+						console.log(avatars);
+						if (!avatars.result.total_count) {
+							console.log('无头像', message.body.userId)
+							message.ack()
+							continue
+						}
+
+						const avatar = avatars.result.photos[0][
+							avatars.result.photos[0].length - 1
+						] as TelegramPhotoSize;
+						await env.AVATAR_META.put(`photoId:${message.body.userId}`, avatar.file_id);
+
+						fileId = avatar.file_id
+						break;
+				}
+				if (await env.ASSETS_STORE.head(`files/${fileId}`)) {
 					console.log('file exists')
 					message.ack()
 					continue;
 				}
-				const bot = new Bot(message.body.botToken)
-				const fileUrl = await bot.getFile(message.body.fileId)
+				console.log('获取文件', fileId)
+				const fileUrl = await bot.getFile(fileId)
 				const file = await fetch(fileUrl);
-				await env.ASSETS_STORE.put(`files/${message.body.fileId}`, file.body)
+				await env.ASSETS_STORE.put(`files/${fileId}`, file.body)
+				message.ack()
 			}
 			catch (e) {
 				console.log(e)
--- src/types/imageFetchRequest.d.ts
+++ src/types/imageFetchRequest.d.ts
@@ -1,5 +1,9 @@
 export type ImageFetchRequest = {
 	botToken: string,
+} & ({
 	type: 'image',
 	fileId: string
-}
+} | {
+	type: 'avatar',
+	userId: number
+})
